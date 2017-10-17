e3c7b6585ca744ce70b280f29a3afba7bba0ebd7
manemal <a_d_quinn@hotmail.com>
2017-10-17 11:13:21 +1000

    - Inclusion of the getFavouriteFilesFromSite method to the ClientCustomisations library

--- src/client-customisations.js
+++ src/client-customisations.js
@@ -111,4 +111,98 @@
             }
         };
     };
+
+    /**
+     * Display a list of the current users Favourite Files.
+     */
+    ClientCustomisations.getFavouriteFilesList = function() {
+
+        var cssDefaultList = "list-unstyled favModalScroll";
+
+        return {
+            forSite: function(siteID) {
+
+                // if user passes anything other than a +ve integer, default to all sites
+                if (_.isNaN(+siteID) || siteID < 1) {
+                    siteID = undefined;
+                }
+
+                return {
+                    placeInto: function($elem) {
+
+                        var lastRowID = _.isUndefined(window["BannertopCollection"].lastRowID) ? 0 : +window["BannertopCollection"].lastRowID;
+
+                        // make sure is valid number - otherwise zero (start)
+                        lastRowID = +lastRowID || 0;
+
+                        // we support user supplying string of jquery selector or just the name of the id
+                        if (!($elem instanceof jQuery)) {
+
+                            // try selector string first
+                            var $elFind = $($elem.toString());
+                            if (!$elFind.length) {
+                                // no element exists with that selector, try prefixing with id (#) selector before failing
+                                $elFind = $("#" + $elem.toString());
+                            }
+
+                            $elem = $elFind;
+                        }
+
+                        // make sure is valid jQuery object before continuing
+                        if (!($elem instanceof jQuery) || !$elem.length) {
+                            console.error("ClientCustomisations - cannot find provided element in the page", $elem);
+                            return ClientCustomisations;
+                        }
+
+                        // make sure we are adding to an <UL> unordered list element
+                        if ($elem.prop("tagName") === "UL") {
+                            $elem.addClass(cssDefaultList);
+                        } else {
+                            $elem = $("<ul></ul>").appendTo($elem).addClass(cssDefaultList);
+                        }
+
+                        if (!lastRowID) {
+                            $elem.empty();
+                        }
+
+                        // Build the url we use to query Collaborate about the user's Favourite documents
+                        var url = "./dashboardFavouriteItemList.action?displayContent=FAVOURITES_ITEM_LIST&metaData.fromAction=FAVOURITE_MODAL_REFRESH&favouritesearch=&filtercontenttype=Document&currentPage=0&rowID={{ROW_ID}}&{{TOKEN_NAME}}={{TOKEN_VALUE}}"
+                            .replace(/{{ROW_ID}}/i, lastRowID)
+                            .replace(/{{TOKEN_NAME}}/i, window.systemProperty["CSRF_TOKEN_NAME"])
+                            .replace(/{{TOKEN_VALUE}}/i, window.systemProperty["CSRF_TOKEN_VALUE"]);
+
+                        $.post(url, {
+                            "check": true,
+                            "metaData.screenWidth": screen.width,
+                            "ts": new Date().valueOf()
+                        }).done(function(html) {
+
+                            // and the returned list to process
+                            var $html = $("<ul></ul>").append(html);
+
+                            // if siteID provided, filter the list to only include...
+                            if (+siteID) {
+                                $html.find(">li").each(function(){
+                                    if (!$(this).find("a[href][siteID='" + siteID + "']").length) {
+                                        $(this).remove();
+                                    }
+                                });
+                            }
+
+                            // add list items to the element user passed in
+                            $elem.append($html.children());
+
+                            // if more results pending, collect until done
+                            if (!window["BannertopCollection"].isStopScrolling) {
+                                ClientCustomisations.getFavouriteFilesList().forSite(siteID).placeInto($elem);
+                            }
+                        });
+
+                        return ClientCustomisations;
+                    }
+                };
+            }
+        }
+    };
+
 })(jQuery.noConflict());
